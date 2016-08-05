fcc609be8d007d5530bb8ebf3509f593b792e21c
admin <admin@iMac-3.local>
2016-08-05 14:56:44 -0400

    Change in data base relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   has_many :collaborations
   has_many :collaboration_lists, through: :collaborations, :source => :list
 
-
-  has_many :tasks, through: :created_lists, :source => :task
-  has_many :collaboration_tasks, :through => :collaboration_lists
+  has_many :tasks
+  has_many :collaboration_tasks, through: :collaboration_lists, :source => :tasks
+  has_many :my_tasks, through: :created_lists, :source => :tasks
 end
